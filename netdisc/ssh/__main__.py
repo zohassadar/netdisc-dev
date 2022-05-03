fecb69d5f998f5c6a3ff252fe124be25231b7279
Richard Dodson <zohassadar@gmail.com>
2022-05-03 13:12:24 -0700

    clean up

--- netdisc/ssh/__main__.py
+++ netdisc/ssh/__main__.py
@@ -1,11 +1,6 @@
 import argparse
 import pprint
-import socket
-
-
-import textfsm
 from netdisc.ssh import custparse
-from ntc_templates import parse
 import logging
 
 logger = logging.getLogger(__name__)
@@ -32,44 +27,6 @@ ssh_test_args.add_argument(
     action="store_true",
     help="Use custom templates",
 )
-
-
-def get_host_by_ip(ip):
-    try:
-        result = socket.gethostbyaddr(ip)
-        logging.debug("Resolved %s to: %s", ip, result)
-        return result[0]
-    except socket.herror as exc:
-        logging.debug("Unable to resolve %s: %s.  Returning IP.", ip, exc)
-        return ip
-
-
-def nexus_mpls_route_report():
-    routes = parse.parse_output("cisco_nxos", "show ip route", SHIPROUTE)
-    mpls_destinations = [r for r in routes if r["nexthop_vrf"]]
-    results = []
-    for mpls_dest in mpls_destinations:
-        result = {}
-        result["network"] = mpls_dest["network"]
-        result["mask"] = mpls_dest["mask"]
-        result["nexthop_ip"] = mpls_dest["nexthop_ip"]
-        result["nexthop_fqdn"] = get_host_by_ip(mpls_dest["nexthop_ip"])
-        results.append(result)
-    return results
-
-
-def arista_vxlan_route_report():
-    routes = parse.parse_output("arista_eos", "show ip route", SHIPROUTE)
-    mpls_destinations = [r for r in routes if r["nexthop_vrf"]]
-    results = []
-    for mpls_dest in mpls_destinations:
-        result = {}
-        result["network"] = mpls_dest["network"]
-        result["mask"] = mpls_dest["mask"]
-        result["nexthop_ip"] = mpls_dest["nexthop_ip"]
-        result["nexthop_fqdn"] = get_host_by_ip(mpls_dest["nexthop_ip"])
-        results.append(result)
-    return results
 
 
 def test_and_pprint(
